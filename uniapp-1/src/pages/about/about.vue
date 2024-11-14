f6fc268a276a949249aa71d88758cb0434cb2041
euphon <eu@fufu.moe>
2024-11-14 17:13:49 +0800

    常用组件学习

---
+++ uniapp-1/src/pages/about/about.vue
@@ -0,0 +1,12 @@
+<template>
+    <view>
+        <text selectable>{{ title }}</text>
+    </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('About')
+</script>
+
+<style scoped lang="scss"></style>
--- uniapp-1/src/pages/index/index.vue
+++ uniapp-1/src/pages/index/index.vue
@@ -1,41 +1,75 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
+    <view>
+        <text selectable>{{ title }}</text>
     </view>
-  </view>
+
+    <view>------</view>
+
+    <scroll-view scroll-y class="scrollView">
+        <view v-for="i in 100" :key="i">元素 {{ i }}</view>
+    </scroll-view>
+
+    <view>------</view>
+
+    <swiper indicator-dots indicator-color="rgba(255,255,255, 0.5)" indicator-active-color="#fff" circular autoplay>
+        <swiper-item v-for="i in 4" :key="i">元素 {{ i }}</swiper-item>
+    </swiper>
+
+    <view>------</view>
+
+    <image src="@/static/logo.png"></image>
+    <image
+        src="../../static/logo.png"
+        mode="scaleToFill"
+    />
+
+    <view>------</view>
+
+    <button size="mini" style="background-color: blue;" plain @click="goto('/pages/about/about')">关于</button>
+
+    <swiper indicator-dots autoplay circular>
+        <swiper-item>hello</swiper-item>
+        <swiper-item>hello</swiper-item>
+        <swiper-item>hello</swiper-item>
+        <swiper-item>hello</swiper-item>
+        <swiper-item>hello</swiper-item>
+    </swiper>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 const title = ref('Hello')
+const goto = (url: string) => {
+    uni.navigateTo({
+        url: url
+    })
+}
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+<style scoped lang="scss">
+.scrollView {
+    width: 80%;
+    height: 100px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid #ccc;
 }
 
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
+swiper {
+    box-sizing: border-box;
+    width: 100vw;
+    height: 200px;
+    border: 1px solid green;
 
-.text-area {
-  display: flex;
-  justify-content: center;
-}
+    swiper-item {
+        width: 100%;
+        height: 100%;
+        background-color: pink;
+        text-align: center;
+    }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+    swiper-item:nth-child(2n) {
+        background-color: orange;
+    }
 }
 </style>
